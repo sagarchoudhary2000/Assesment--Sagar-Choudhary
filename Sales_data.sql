5f7f9641c345099ff63aa8907c56bc6b19040f52
Sagar Ganesh  Chaudhari <sagargchoudhary2000@gmail.com>
2025-02-14 12:26:30 +0530

    Analyze unique customers and average customer age

--- Sales_data.sql
+++ Sales_data.sql
@@ -3,3 +3,8 @@ SELECT SUM(TransactionAmount) AS Total_Revenue,
        COUNT(TransactionID) AS Total_Transactions, 
        AVG(TransactionAmount) AS Avg_Transaction 
 FROM sales_data;
+
+-- Step 2: Customer Insights
+SELECT COUNT(DISTINCT CustomerID) AS Unique_Customers, 
+       AVG(CustomerAge) AS Avg_Customer_Age 
+FROM sales_data;
